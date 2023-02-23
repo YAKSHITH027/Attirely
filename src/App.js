ee5e8859ebe76ebbc7176f15e7475d549b658f79
codersudhir <nandanesudhir1@gmail.com>
2023-02-23 22:32:33 +0530

    dfg

--- src/App.js
+++ src/App.js
@@ -3,13 +3,17 @@ import Home from "./UserSide/Pages/Home";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import Dashboard from "./AdminSide/Pages/Dashboard";
+import Cart from "../src/UserSide/Pages/Cart/Cart"
 
 
 function App() {
   return (
     <div className="App">
       {/* <Dashboard /> */}
-      <Home />
+    
+      {/* <Home /> */}
+      {/* <Cart/> */}
+  
     </div>
   );
 
--- src/index.js
+++ src/index.js
@@ -1,9 +1,7 @@
 import { ChakraProvider } from "@chakra-ui/react";
 import React from "react";
 import ReactDOM from "react-dom/client";
-
 import App from "./App";
-import { ChakraProvider } from "@chakra-ui/react";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
