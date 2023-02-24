d228012ee3ee7b1c06fa0ede78c522259bb9af74
codersudhir <nandanesudhir1@gmail.com>
2023-02-24 11:50:52 +0530

    gg

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import Home from "./UserSide/Pages/Home";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import Dashboard from "./AdminSide/Pages/Dashboard";
-import Cart from "../src/UserSide/Pages/Cart/Cart"
 
 
 function App() {
@@ -12,7 +11,7 @@ function App() {
       {/* <Dashboard /> */}
     
       {/* <Home /> */}
-      {/* <Cart/> */}
+     
   
     </div>
   );
