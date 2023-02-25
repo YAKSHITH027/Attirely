f755993aa12939b66596e684a9ad782a93fc777c
Yakshith <yakshith027@gmail.com>
2023-02-25 21:19:07 +0530

    status api added

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
 import { AllRoutes } from "./lib/AllRoutes";
+import Dummy from "./Hooks/dummy";
 
 function App() {
   return (
     <div className="App">
+      <Dummy />
       <AllRoutes />
     </div>
   );
