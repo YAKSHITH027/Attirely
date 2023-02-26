28f188d9d045b77de95efdf22874e8f588adf40b
TrilochanprasadBH <104064013+TrilochanprasadBH@users.noreply.github.com>
2023-02-26 16:39:26 +0530

    sellers

--- src/AdminSide/Pages/Sellers.jsx
+++ src/AdminSide/Pages/Sellers.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
+import { SellerData } from '../../Utils/SellerData'
+import { Box , Image, Text} from "@chakra-ui/react"
+import SellersMapbox from "./SellersMapbox";
 
+console.log("sellers")
 const Sellers = () => {
+  
   return (
-    <div>Sellers</div>
+    <div style={{display:"grid", gridTemplateColumns:"repeat(3,1fr)"}}>
+      {SellerData.map((item)=>(
+        <SellersMapbox props={item}/>
+      ))}
+    </div>
   )
+
 }
 
 export default Sellers
