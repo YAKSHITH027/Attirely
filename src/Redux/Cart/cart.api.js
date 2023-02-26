dcae4b04e7e1af305e066ba0f855fe24a84d24c2
Yakshith <yakshith027@gmail.com>
2023-02-26 17:01:30 +0530

    check it

--- src/Redux/Cart/cart.api.js
+++ src/Redux/Cart/cart.api.js
@@ -13,7 +13,7 @@ import { db } from "../../lib/firebase";
 export const cartAPI = async (id) => {
   try {
     const citiesRef = collection(db, "cart");
-    const q = query(citiesRef, where("id", "==", id));
+    const q = query(citiesRef, where("userId", "==", id));
     const querySnapshot = await getDocs(q);
     let result;
     querySnapshot.forEach((doc) => {
